f1a1d60f101b13d0efed5fe02f6392bcf18b2af1
Tomas Iglesias <toiglesias@est.frba.utn.edu.ar>
2020-05-14 19:19:40 -0300

    correcion issue 1

--- tp3.hs
+++ tp3.hs
@@ -22,23 +22,29 @@ carolina = CrearParticipante "Carolina" 500 "Accionista" [] [pasarPorElBanco, pa
 manuel :: Participante
 manuel = CrearParticipante "Manuel" 500 "Oferente singular" [] [pasarPorElBanco, enojarse] 
 
+cambiarDinero :: Int -> Accion
+cambiarDinero cantidadDinero participante = participante {dinero = dinero participante + cantidadDinero }
+
+cambiarTactica :: String -> Accion
+cambiarTactica nuevaTactica participante = participante {tactica = nuevaTactica}
+
 pasarPorElBanco :: Accion
-pasarPorElBanco participante = participante {dinero = dinero participante + 40, tactica = "Comprador Compulsivo"}
+pasarPorElBanco participante = ((cambiarDinero 40). (cambiarTactica "Comprador Compulsivo")) participante
 
 enojarse :: Accion
-enojarse participante = participante {dinero = dinero participante +50, acciones = acciones participante ++ [gritar] }
+enojarse participante = cambiarDinero 50 participante { acciones = acciones participante ++ [gritar] }
 
 gritar :: Accion
 gritar participante = participante {nombre = "AHHHH " ++ nombre participante}
 
--- como se haria por pattern mattching??
+-- como se haria por pattern mattching?
 subastar :: Propiedad -> Accion
 subastar propiedadAAdquirir participante 
     | esAccionistaUOferente participante = ganarPropiedad propiedadAAdquirir participante
     | otherwise = participante
 
 ganarPropiedad :: Propiedad -> Accion
-ganarPropiedad propiedadGanada participante = participante { dinero = dinero participante - (snd propiedadGanada), propiedades = propiedades participante ++ [propiedadGanada] }
+ganarPropiedad propiedadGanada participante = cambiarDinero (snd propiedadGanada) participante {propiedades = propiedades participante ++ [propiedadGanada] }
 
 esAccionistaUOferente :: Participante -> Bool
 esAccionistaUOferente participante = tactica participante == "Oferente singular" || tactica participante == "Accionista" 
@@ -46,10 +52,10 @@ esAccionistaUOferente participante = tactica participante == "Oferente singular"
 -- como se haria por pattern mattching?
 pagarAAccionistas :: Accion 
 pagarAAccionistas participante 
-    | tactica participante == "Accionista" = participante { dinero = dinero participante + 200 }
-    | otherwise = participante { dinero = dinero participante - 100 }
+    | tactica participante == "Accionista" = cambiarDinero 200 participante
+    | otherwise = cambiarDinero (-100) participante
 
-
+--esta issue todavia no la modifique. 
 cobrarAlquileres :: Accion
 cobrarAlquileres participante = participante { dinero = dinero participante + (sum.(map esPropiedadBarata).propiedades) participante }
 
